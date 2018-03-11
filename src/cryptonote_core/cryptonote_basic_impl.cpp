d7b80a4711c1ab7a3cb9d499687236e446cb26b7
anythingtechpro <anythingtechpro@gmail.com>
2018-03-11 18:47:18 -0400

    cryptonote_core: fixed an issue when calculating the block reward on x32 based machines

--- src/cryptonote_core/cryptonote_basic_impl.cpp
+++ src/cryptonote_core/cryptonote_basic_impl.cpp
@@ -84,7 +84,9 @@ namespace cryptonote {
     assert(current_block_size < std::numeric_limits<uint32_t>::max());
 
     uint64_t product_hi;
-    uint64_t product_lo = mul128(base_reward, current_block_size * (2 * median_size - current_block_size), &product_hi);
+    uint64_t multiplicand = 2 * median_size - current_block_size;
+    multiplicand *= current_block_size;
+    uint64_t product_lo = mul128(base_reward, multiplicand, &product_hi);
 
     uint64_t reward_hi;
     uint64_t reward_lo;
